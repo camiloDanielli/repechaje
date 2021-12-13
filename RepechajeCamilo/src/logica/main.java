2b283d7399993121be33f3aa45d748593654c70a
camiloDanielli <79849665+camiloDanielli@users.noreply.github.com>
2021-12-13 14:20:10 -0300

    Add files via upload

---
+++ RepechajeCamilo/src/logica/main.java
@@ -0,0 +1,19 @@
+package logica;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		// TODO Auto-generated method stub
+		System.out.println("***********************************************");
+		System.out.println("bienvenido al gestor de usuarios y materias");
+		System.out.println("*****************************************");
+		System.out.println("1-gestionar alumnos");
+		System.out.println("2-gestionar profesores");
+		System.out.println("3-gestionar materias");
+		String selecion = in.nextLine();
+	}
+
+}
